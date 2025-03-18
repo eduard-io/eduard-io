3f50ae0dc37958a61b42aa313daa5407f6837aea
Eduard Giménez <eduard@eduard.io>
2025-03-19 00:14:05 +0100

    chore: add layout and includes

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,1 +1,27 @@
 // This is where it all goes :)
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('theme', {
+    current: localStorage.getItem('theme') || 'system',
+    
+    init() {
+      this.applyTheme(this.current);
+    },
+
+    toggle() {
+      const next = this.current === 'dark' ? 'light' : 'dark';
+      this.applyTheme(next);
+      this.current = next;
+    },
+
+    applyTheme(theme) {
+      if (theme === 'system') {
+        document.documentElement.classList.remove('light', 'dark');
+      } else {
+        document.documentElement.classList.remove('light', 'dark');
+        document.documentElement.classList.add(theme);
+      }
+      localStorage.setItem('theme', theme);
+    }
+  });
+});
