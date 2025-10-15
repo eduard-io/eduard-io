285e858da4d3a1096baecf4f885b9caa313aefd3
Eduard Giménez <eduard@eduard.io>
2025-10-15 15:29:50 +0200

    fix: publish robots.txt

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./assets/'); // Watch /assets for changes
   eleventyConfig.addPassthroughCopy('assets/');
+  eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' });
   // eleventyConfig.addPassthroughCopy("assets/images"); // Ensure images are copied to _site
   // eleventyConfig.addPassthroughCopy("assets/javascript"); // Ensure images are copied to _site
   // eleventyConfig.addPassthroughCopy("assets/stylesheets"); // Ensure images are copied to _site
